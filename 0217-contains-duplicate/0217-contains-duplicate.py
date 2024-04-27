a64abcd742152f00cb3050b3d978137482c53244
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-04-27 16:16:18 -0400

    Time: 418 ms (59.90%), Space: 31.8 MB (86.53%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,8 +1,10 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
+class Solution: 
+    def containsDuplicate(self, nums:List[int]) -> bool:
         hashset = set()
-        for n in nums:
-            if n in hashset:
-                return True
-            hashset.add(n)
+        for i in nums:
+            if i in hashset:
+                return True 
+            hashset.add(i)
         return False
+            
+
