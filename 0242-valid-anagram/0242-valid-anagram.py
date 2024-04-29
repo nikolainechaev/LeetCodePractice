a3564db31c31736f11a5af80e51b9f8956f7ca21
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-04-29 15:10:43 -0400

    Time: 58 ms (16.34%), Space: 16.9 MB (64.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): 
+        sDictinary, tDictinary = {}, {}
+        if len(s) != len(t):
             return False
-        countS, countT = {}, {}
 
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        for c in countS:
-            if countS[c] != countT.get(c, 0):
+        for char in range(len(s)):
+            sDictinary[s[char]] = 1 + sDictinary.get(s[char], 0)
+            tDictinary[t[char]] = 1 + tDictinary.get(t[char], 0)
+
+        for l in sDictinary:
+            if sDictinary[l] != tDictinary.get(l, 0):
                 return False
         return True
-
