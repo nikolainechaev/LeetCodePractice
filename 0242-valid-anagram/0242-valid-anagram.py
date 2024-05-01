82c5c81ae66212634839b8a749b42838db459196
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-04-30 22:12:28 -0400

    Time: 48 ms (63.73%), Space: 17 MB (33.80%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,16 +1,12 @@
 class Solution: 
     def isAnagram(self, s: str, t: str) -> bool:
-
         if len(s) != len(t):
-            return False 
-
-        sDict, tDict = {}, {}
-        
-        for i in range(len(s)):
-            sDict[s[i]] = 1 + sDict.get(s[i], 0)
-            tDict[t[i]] = 1 + tDict.get(t[i], 0)
-
-        for c in sDict:
-            if sDict[c] != tDict.get(c, 0):
+            return False
+        dictS, dictT = {}, {}
+        for l in range(len(s)):
+            dictS[s[l]] = 1 + dictS.get(s[l], 0)
+            dictT[t[l]] = 1 + dictT.get(t[l], 0)
+        for c in dictS:
+            if dictS[c] != dictT.get(c, 0):
                 return False
         return True
