fe1405345b1c60ada778d8bb7fb6319c4673bb4c
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-06-03 18:35:52 -0400

    Time: 77 ms (96.18%), Space: 22.3 MB (13.93%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,64 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        finalList = defaultdict(list)  # creates list: list dict
+        dict = defaultdict(list) # creates dict
         
-        for string in strs:
-            alphabetList = [0] * 26    # creates a-z placeholders
+        for str in strs:
+            alphabet = [0] * 28 # creates a-z placeholders
+            for char in str:
+                alphabet[ord(char) - ord('a')] += 1
+                
+            dict[tuple(alphabet)].append(str)
+        return dict.values()
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+#                       , strs: List[str]) -> List[List[str]]:
+        
+#         finalList = defaultdict(list)  # creates list dict
+        
+#         for string in strs:
+#             alphabetList = [0] * 26    # creates a-z placeholders
             
-            for letter in string:
-                alphabetList[ord(letter) - ord('a')] += 1 # insert 1s in alphabet order for each char into alphabetList
+#             for letter in string:
+#                 alphabetList[ord(letter) - ord('a')] += 1 # insert 1s in alphabet order for each char into alphabetList
         
-            finalList[tuple(alphabetList)].append(string) 
-# take finalList and create keys out of list from alphabetList and assign related string
+#             finalList[tuple(alphabetList)].append(string) 
+# # take finalList and create keys out of list from alphabetList and assign related string
         
-        return finalList.values() # return only values within one list
+#         return finalList.values() # return only values within one list
 
 
