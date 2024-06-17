18567d853c46017c58645ae7a2aad49bf866737a
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-06-17 14:19:32 -0400

    Time: 101 ms (10.64%), Space: 21.9 MB (22.32%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,44 +1,18 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k:int) -> List[int]: 
+    def topKFrequent(self, nums:List[int], k:int) -> List[int]:
+        
         dict = {}
         list = [[] for i in range(len(nums) + 1)]
         
-        for number in nums: 
-            dict[number] = 1 + dict.get(number, 0)
-        for number, quantity in dict.items():
-            list[quantity].append(number)    # why we can't use just list[quantity] = number ???
+        for num in nums:
+            dict[num] = 1 + dict.get(num, 0)
+        for num, quant in dict.items():
+            list[quant].append(num)
         
-        response = []
-        for i in range(len(list) -1, 0, -1): # need to remember the order of those -1,0,-1 and what they mean
-            for number in list[i]:
-                response.append(number)
-                if len(response) == k:
-                    return response
-            
-            
-
-
-
-
-
-
-
-
-
-
-# class Solution:
-#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-#         count = {}
-#         freq = [[] for i in range(len(nums) + 1)]
         
-#         for number in nums:
-#             count[number] = 1 + count.get(number, 0) # will create a dict that will look like this {1: 3, 2: 1, ... }
-#         for number, quantity in count.items():       # will take each pair and create a tuple
-#             freq[quantity].append(number)            # will make freq list with reversed variation of count [(3: 1), (1: 2), ...]
-            
-#         response = []                                # empty list
-#         for i in range(len(freq) -1, 0, -1):         # taking freq list and iterate from last item in the list 
-#             for number in freq[i]:                   # in freq list we iterate over keys in tuples
-#                 response.append(number)              # append each number to response list, until we reach k quantity in response
-#                 if len(response) == k:
-#                     return response
+        finalResult = []
+        for num in range(len(list)-1, 0, -1):
+            for value in list[num]:
+                finalResult.append(value)
+                if len(finalResult) == k:
+                    return finalResult
