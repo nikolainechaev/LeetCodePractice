8d1910d4b5eda300d99e18c06aa3d0dfc7340259
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-06-22 20:46:21 -0400

    Time: 94 ms (30.94%), Space: 22 MB (21.51%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,17 +2,21 @@ class Solution:
     def topKFrequent(self, nums:List[int], k:int) -> List[int]:
         
         dict = {}
-        list = [[] for i in range(len(nums) + 1)]
+        list = [[] for i in range(len(nums) + 1)] #forgot about "+1"
         
-        for num in nums:
-            dict[num] = 1 + dict.get(num, 0)
-        for num, quant in dict.items():
-            list[quant].append(num)
+        for number in nums:
+            dict[number] = 1 + dict.get(number, 0) #forgot about "dict.get(number, 0)" also used "+=" instead of "="
+        for quantity, number in dict.items():  #forgot about ".items()"
+            list[number].append(quantity)
+        
+        results = []
+        
+        for i in range(len(list) -1, 0, -1):  #forgot about "len()"
+            for j in list[i]:
+                results.append(j)
+                if len(results) == k:
+                    return results
+            
+            
         
         
-        finalResult = []
-        for num in range(len(list)-1, 0, -1):
-            for value in list[num]:
-                finalResult.append(value)
-                if len(finalResult) == k:
-                    return finalResult
