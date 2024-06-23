b07a9a2466c8d497466312286ac7a7afc854be65
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-06-23 10:28:59 -0400

    Time: 97 ms (19.83%), Space: 21.8 MB (31.39%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,22 +1,14 @@
 class Solution:
-    def topKFrequent(self, nums:List[int], k:int) -> List[int]:
-        
-        dict = {}
-        list = [[] for i in range(len(nums) + 1)] #forgot about "+1"
-        
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dict, list = {}, [[] for i in range(len(nums) + 1)]
         for number in nums:
-            dict[number] = 1 + dict.get(number, 0) #forgot about "dict.get(number, 0)" also used "+=" instead of "="
-        for number, quantity in dict.items():  #forgot about ".items()", also forgot about what goes first quantity or number...
+            dict[number] = 1 + dict.get(number, 0)
+        for number, quantity in dict.items():
             list[quantity].append(number)
-        
-        results = []
-        
-        for i in range(len(list) -1, 0, -1):  #forgot about "len()"
-            for j in list[i]:
-                results.append(j)
-                if len(results) == k:
-                    return results
-            
-            
-        
-        
+        result = []
+        for i in range(len(list) -1, 0, -1):
+            for number in list[i]:
+                result.append(number)
+                if len(result) == k:
+                    return result
+                
