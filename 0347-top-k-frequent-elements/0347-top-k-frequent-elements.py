4e7946efe4ca90c28fbea6aff4ff15cb56f0531d
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-06-24 21:24:06 -0400

    Time: 99 ms (15.09%), Space: 22 MB (21.41%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,35 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        dict, list = {}, [[] for i in range(len(nums) + 1)]
-        for number in nums:
+        dict = {}
+        list = [[] for i in range(len(nums) + 1)]
+        results = []
+    
+        for number in nums: 
             dict[number] = 1 + dict.get(number, 0)
+    
         for number, quantity in dict.items():
             list[quantity].append(number)
-        result = []
-        for i in range(len(list) -1, 0, -1):
+
+        for i in range(len(list) -1, 0, -1): 
+            # Mistake: iterrated through "nums", but need to iterate through "list"
+            #Also forgot to create another iteration for number in list[i]:
             for number in list[i]:
-                result.append(number)
-                if len(result) == k:
-                    return result
+                results.append(number)
+                if len(results) == k: # Used "2" instead of "k"
+                    return results
+        
+        
+        
+        
+#         dict, list = {}, [[] for i in range(len(nums) + 1)]
+#         for number in nums:
+#             dict[number] = 1 + dict.get(number, 0)
+#         for number, quantity in dict.items():
+#             list[quantity].append(number)
+#         result = []
+#         for i in range(len(list) -1, 0, -1):
+#             for number in list[i]:
+#                 result.append(number)
+#                 if len(result) == k:
+#                     return result
                 
