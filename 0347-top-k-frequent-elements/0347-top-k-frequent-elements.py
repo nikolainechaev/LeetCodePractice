ff2a201ae42b9eb0054c53246bc73f20df715b08
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-06-25 21:56:12 -0400

    Time: 86 ms (72.02%), Space: 22 MB (21.29%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,21 +2,62 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         dict = {}
         list = [[] for i in range(len(nums) + 1)]
-        results = []
-    
-        for number in nums: 
+        
+        for number in nums:
             dict[number] = 1 + dict.get(number, 0)
-    
-        for number, quantity in dict.items():
-            list[quantity].append(number)
-
-        for i in range(len(list) -1, 0, -1): 
-            # Mistake: iterrated through "nums", but need to iterate through "list"
-            #Also forgot to create another iteration for number in list[i]:
-            for number in list[i]:
-                results.append(number)
-                if len(results) == k: # Used "2" instead of "k"
+        for number, q in dict.items():
+            list[q].append(number)
+        
+        results = []
+        
+        for i in range(len(list) -1, 0, -1):
+            for j in list[i]:
+                results.append(j)
+                if len(results) == k:
                     return results
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#         dict = {}
+#         list = [[] for i in range(len(nums) + 1)]
+#         results = []
+    
+#         for number in nums: 
+#             dict[number] = 1 + dict.get(number, 0)
+    
+#         for number, quantity in dict.items():
+#             list[quantity].append(number)
+
+#         for i in range(len(list) -1, 0, -1): 
+#             # Mistake: iterrated through "nums", but need to iterate through "list"
+#             #Also forgot to create another iteration for number in list[i]:
+#             for number in list[i]:
+#                 results.append(number)
+#                 if len(results) == k: # Used "2" instead of "k"
+#                     return results
         
         
         
