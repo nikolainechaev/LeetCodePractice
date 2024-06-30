5ef33b726d2d8825585c7e67ac1db9dd7871e80a
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-06-30 07:08:46 -0400

    Time: 89 ms (56.51%), Space: 21.8 MB (31.85%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,94 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-
-        dict, list, result = {}, [[] for i in range(len(nums) +1)], []
-        
-        for number in nums: 
-            dict[number] = 1 + dict.get(number, 0)
-        for number, quantity in dict.items():
-            list[quantity].append(number)
-        for i in range(len(list) -1, 0, -1):
-            for j in list[i]:
-                result.append(j)
-                if len(result) == k:
-                    return result
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-
-
-
-#         dict = {}
-#         list = [[] for i in range(len(nums) + 1)]
-        
-#         for number in nums:
-#             dict[number] = 1 + dict.get(number, 0)
-#         for number, q in dict.items():
-#             list[q].append(number)
-        
-#         results = []
-        
-#         for i in range(len(list) -1, 0, -1):
-#             for j in list[i]:
-#                 results.append(j)
-#                 if len(results) == k:
-#                     return results
+        count, res, list = {}, [], [[] for i in range(len(nums) + 1)] #forgot "+1"
+        for number in nums:
+            count[number] = 1 + count.get(number, 0)
+        for number, count in count.items():
+            list[count].append(number)
+        for number in range(len(list) -1, 0, -1):
+            for j in list[number]:  #forgot about whole this iteration line
+                res.append(j)
+                if len(res) == k:
+                    return res
             
-
-
-
-
-
-
-#         dict = {}
-#         list = [[] for i in range(len(nums) + 1)]
-#         results = []
-    
-#         for number in nums: 
-#             dict[number] = 1 + dict.get(number, 0)
-    
-#         for number, quantity in dict.items():
-#             list[quantity].append(number)
-
-#         for i in range(len(list) -1, 0, -1): 
-#             # Mistake: iterrated through "nums", but need to iterate through "list"
-#             #Also forgot to create another iteration for number in list[i]:
-#             for number in list[i]:
-#                 results.append(number)
-#                 if len(results) == k: # Used "2" instead of "k"
-#                     return results
-        
-        
-        
-        
-#         dict, list = {}, [[] for i in range(len(nums) + 1)]
-#         for number in nums:
-#             dict[number] = 1 + dict.get(number, 0)
-#         for number, quantity in dict.items():
-#             list[quantity].append(number)
-#         result = []
-#         for i in range(len(list) -1, 0, -1):
-#             for number in list[i]:
-#                 result.append(number)
-#                 if len(result) == k:
-#                     return result
-                
+            
