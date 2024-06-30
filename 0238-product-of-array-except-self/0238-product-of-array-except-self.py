02df07013c9f45bcce16f5ccf39c75020d5e2320
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-06-29 20:07:38 -0400

    Time: 277 ms (36.68%), Space: 25.7 MB (52.68%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,12 +1,29 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * len(nums)
+        result = [1] * len(nums)
         
         for i in range(1, len(nums)):
-            res[i] = res[i - 1] * nums[i - 1]
+            result[i] = result[i-1] * nums[i-1] # where is sum of prefixes here?
         postfix = 1
         for i in range(len(nums) -1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-        return res
+            result[i] *= postfix
+            postfix *= nums[i] #forgot that we need to update postfix variable with number from "nums"
+        return result
+        
+        
+        
+        
+        
+        
+        
+        
+#         res = [1] * len(nums)
+        
+#         for i in range(1, len(nums)):
+#             res[i] = res[i - 1] * nums[i - 1]
+#         postfix = 1
+#         for i in range(len(nums) -1, -1, -1):
+#             res[i] *= postfix
+#             postfix *= nums[i]
+#         return res
             
