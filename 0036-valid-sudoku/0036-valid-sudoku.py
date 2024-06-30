edda3e4457d1cf18147840ec5c9d6124ea68c90a
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-06-30 07:46:38 -0400

    Time: 98 ms (22.75%), Space: 16.5 MB (81.56%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,20 +1,20 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        columns = defaultdict(set)
+        cols = defaultdict(set)
         rows = defaultdict(set)
-        squares = defaultdict(set)
+        sqrs = defaultdict(set)
         
         for r in range(9):
             for c in range(9):
                 if board[r][c] == '.':
                     continue
-                if (
-                    board[r][c] in rows[r] 
-                    or board[r][c] in columns[c]
-                    or board[r][c] in squares[(r // 3, c // 3)]
-                ): 
+                if(
+                    board[r][c] in cols[c] or #forgot about index for cols -> "cols[c]"
+                    board[r][c] in rows[r] or #forgot about index for rows -> "rows[r]"
+                    board[r][c] in sqrs[r//3, c//3] #forgot about "[r//3, c//3]"
+                ):
                     return False
-                rows[r].add(board[r][c])
-                columns[c].add(board[r][c])
-                squares[(r // 3, c // 3)].add(board[r][c])
+                cols[c].add(board[r][c]) #forgot that i should add number to specific [c]
+                rows[r].add(board[r][c]) #forgot that i should add number to specific [r]
+                sqrs[r//3, c//3].add(board[r][c]) #forgot that i should add number to specific [r//3, c//3]
         return True
