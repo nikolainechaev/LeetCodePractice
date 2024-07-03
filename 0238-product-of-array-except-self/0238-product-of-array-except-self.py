57250ac83b191a7009921ccad0d8d799e0ca004d
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-07-03 19:48:06 -0400

    Time: 267 ms (66.10%), Space: 25.6 MB (90.99%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,14 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        result = [1] * len(nums)
+        results = [1] * len(nums)
         
-        for i in range(1, len(nums)):
-            result[i] = result[i-1] * nums[i-1] # where is sum of prefixes here?
+        for number in range(1, len(nums)):
+            results[number] = results[number - 1] * nums[number - 1]
+            
         postfix = 1
-        for i in range(len(nums) -1, -1, -1):
-            result[i] *= postfix
-            postfix *= nums[i] #forgot that we need to update postfix variable with number from "nums"
-        return result
+        for number in range(len(nums) -1, -1, -1):
+            results[number] *= postfix
+            postfix *= nums[number]
+            
+        return results
         
         
         
@@ -17,13 +19,28 @@ class Solution:
         
         
         
-#         res = [1] * len(nums)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         result = [1] * len(nums)
         
 #         for i in range(1, len(nums)):
-#             res[i] = res[i - 1] * nums[i - 1]
+#             result[i] = result[i-1] * nums[i-1]
 #         postfix = 1
 #         for i in range(len(nums) -1, -1, -1):
-#             res[i] *= postfix
+#             result[i] *= postfix
 #             postfix *= nums[i]
-#         return res
-            
+#         return result
