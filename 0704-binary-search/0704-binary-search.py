0acebbd6ec6c7cf884899b536be73d1570d2fe59
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-07-27 06:46:54 -0400

    Time: 197 ms (21.91%), Space: 18 MB (89.66%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -4,10 +4,10 @@ class Solution:
         
         while l <= r:
             m = (l + r) // 2
-            if nums[m] < target:
-                l = m + 1
-            elif nums[m] > target:
+            if nums[m] > target:
                 r = m - 1
+            elif nums[m] < target:
+                l = m + 1
             else:
                 return m
         return -1
