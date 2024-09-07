b3b0899d064acdadcb12f447bae4a2f179f44994
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-09-07 10:21:47 -0400

    Time: 66 ms (12.53%), Space: 16.6 MB (40.86%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,7 +4,7 @@ class Solution:
         
         l, r = 0, len(strNumber) - 1
         
-        while l <= r: 
+        while l < r: 
             if strNumber[l] != strNumber[r]:
                 return False
             l, r = l + 1, r - 1
