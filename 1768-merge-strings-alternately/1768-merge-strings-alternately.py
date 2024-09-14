f4e953811ffa4bf456d7e7a65e6b6d9179208554
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-09-14 05:23:00 -0400

    Time: 33 ms (72.69%), Space: 16.4 MB (72.23%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        length1 = len(word1)
+        length2 = len(word2)
+        pointer1 = 0
+        pointer2 = 0
+        result = []
+        
+        while pointer1 < length1 or pointer2 < length2:
+            if pointer1 < length1:
+                result += word1[pointer1]
+                pointer1 += 1
+            if pointer2 < length2:
+                result += word2[pointer2]
+                pointer2 += 1
+        return ''.join(result)
