56a2c78c453efe9b0dd0a241e0356cd85bb061a6
Nikolai Nechaev <40309434+nikolainechaev@users.noreply.github.com>
2025-04-22 11:25:27 -0400

    Time: 19 ms (37.09%), Space: 22.7 MB (25.25%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-        storage = defaultdict(list)
-
+        bucket = defaultdict(list) 
         for string in strs:
-            alphaKey = [0] * 26
+            alphabet = [0] * 26 # [0, 0, 0, ....]
             for char in string:
-                alphaKey[ord(char) - ord("a")] += 1
-            storage[tuple(alphaKey)].append(string)
-        return list(storage.values())
+                alphabet[ord(char) - ord('a')] += 1
+            bucket[tuple(alphabet)].append(string)
+        return list(bucket.values())
